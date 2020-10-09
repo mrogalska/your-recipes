8864dcd609bf5c7f74844604a5388429571ee1af
Magdalena Rogalska <magda.rogalska.144@gmail.com>
2020-10-09 13:39:58 +0200

    View recipes

--- src/app/models/recipe-type.ts
+++
@@ -1,8 +0,0 @@
-export enum RecipeType {
-    Breakfast,
-    Lunch,
-    Salads,
-    Dinner,
-    Desserts,
-    Drinks
-}
---
+++ src/models/recipe-type.ts
@@ -0,0 +1,8 @@
+export enum RecipeType {
+    Breakfast = 0,
+    Lunch = 1,
+    Salads = 2,
+    Dinner = 3,
+    Desserts = 4,
+    Drinks = 5
+}
