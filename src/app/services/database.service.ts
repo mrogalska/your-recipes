32caa1a39cd04eb97b0012b5da3965e84a725497
Magdalena Rogalska <magda.rogalska.144@gmail.com>
2020-11-16 15:59:10 +0100

    Add database service

---
+++ src/app/services/database.service.ts
@@ -0,0 +1,61 @@
+import { Platform } from '@ionic/angular';
+import { Injectable } from '@angular/core';
+import { SQLitePorter } from '@ionic-native/sqlite-porter/ngx';
+import { HttpClient } from '@angular/common/http';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Recipe } from "../../models/recipe"; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService {
+  private database: SQLiteObject;
+  private dbReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
+ 
+  recipes = new BehaviorSubject([]);
+  ingridients = new BehaviorSubject([]);
+
+  constructor(private plt: Platform, private sqlitePorter: SQLitePorter, private sqlite: SQLite, private http: HttpClient) {
+    this.plt.ready().then(() => {
+      this.sqlite.create({
+        name: 'recipes.db',
+        location: 'default'
+      })
+      .then((db: SQLiteObject) => {
+          this.database = db;
+          this.seedDatabase();
+      });
+    });
+  }
+
+  seedDatabase() {
+    this.http.get('assets/seed.sql', { responseType: 'text'})
+    .subscribe(sql => {
+      this.sqlitePorter.importSqlToDb(this.database, sql)
+        .then(_ => {
+          //  these functions will be implemented
+          this.loadRecipes();
+          this.loadIngridients();
+          this.dbReady.next(true);
+        })
+        .catch(e => console.error(e));
+    });
+  }
+
+  getDatabaseState() {
+    return this.dbReady.asObservable();
+  }
+ 
+  getDevs(): Observable<Recipe[]> {
+    return this.recipes.asObservable();
+  }
+ 
+  getProducts(): Observable<any[]> {
+    return this.ingridients.asObservable();
+  }
+
+  loadRecipes(){}
+
+  loadIngridients(){}
+}
