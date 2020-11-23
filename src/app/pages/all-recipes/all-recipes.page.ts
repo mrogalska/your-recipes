8573346dfdcd91be54662329241d43fd55caff13
Magdalena Rogalska <magda.rogalska.144@gmail.com>
2020-11-23 07:07:20 +0100

    Add functions loading data from database

--- src/app/pages/all-recipes/all-recipes.page.ts
+++ src/app/pages/all-recipes/all-recipes.page.ts
@@ -1,4 +1,6 @@
+import { DatabaseService} from './../../services/database.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from 'src/models/recipe';
 import { RecipeService } from "src/app/services/recipe.service";
@@ -10,25 +12,23 @@ import { RecipeService } from "src/app/services/recipe.service";
 })
 export class AllRecipesPage implements OnInit {
   public recipeType: string;
-  recipes: Recipe[];
+  recipes: Recipe[] = [];
 
   constructor(
     private _recipeService: RecipeService,
     private _activatedRoute: ActivatedRoute,
-    private _router: Router
+    private _router: Router,
+    private _db: DatabaseService
     ) { }
 
   ngOnInit() {
-    this.recipeType = this._activatedRoute.snapshot.paramMap.get('id');
-    this.getRecipes();
-
-  }
-
-  // get data from service
-  
-  getRecipes(): void {
-    this._recipeService.getRecipes()
-        .subscribe(recipes => this.recipes = recipes);
+    this._db.getDatabaseState().subscribe(rdy => {
+      if (rdy) {
+        this._db.getRecipes().subscribe(recipes => {
+          this.recipes = recipes;
+        })
+      }
+    });
   }
 
 
